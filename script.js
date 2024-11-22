a9713a5dddac69cd7cf15a503f74c3cddc5874d6
shaluydv01 <ydvshalu420@gmail.com>
2024-11-22 13:02:44 +0530

    my-portfolio

--- script.js
+++ script.js
@@ -41,3 +41,19 @@ function changeAboutMeText() {
 
 //Call function to add stunning modification
 changeAboutMeText();
+
+
+/*-----------------------------Light/Dark Mode----------------*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  const darkModeToggle = document.getElementById('dark-mode-toggle');
+  const body = document.body;
+
+  darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    const currentMode = body.classList.contains('dark-mode') ? 'Dark' : 'Light' ;
+    darkModeToggle.querySelector('i').classList.toggle('fa-sun');  //Change icon
+    darkModeToggle.querySelector('i').classList.toggle('fa-moon');  //Change icon
+    darkModeToggle.querySelector('i').toggle('light-mode');         //Change icon color
+  });
+});
