22e3d2601fecad07bce43ec582f1bcbc6a7d2b3f
shaluydv01 <ydvshalu420@gmail.com>
2024-11-25 00:03:26 +0530

    my-portfolio

--- Project1/project1.js
+++ Project1/project1.js
@@ -1,14 +1,14 @@
 const Project1Image = document.querySelector(".image-p1");
 
 Project1Image.addEventListener("click", () => {
-  Project1Image.src = "/assets/img/loginPage.png";
+  Project1Image.src = "../assets/img/loginPage.png";
 });
 
 Project1Image.addEventListener("mouseover", () => {
-  Project1Image.src = "/assets/img/signUpPage.png";
+  Project1Image.src = "../assets/img/signUpPage.png";
 });
 
 Project1Image.addEventListener("mouseleave", () => {
-  Project1Image.src = "/assets/img/projectHome.png";
+  Project1Image.src = "../assets/img/projectHome.png";
 });
 
